6ac2fad1592c1fe63cc62a4e8fa84364a8b996d8
vero <veroo.corvalan@gmail.com>
2023-07-11 21:18:17 -0300

    c
    c

---
+++ Clase 15/index.js
@@ -0,0 +1,34 @@
+
+let nombre = document.getElementById("nombre").value;
+const div1 = document.querySelector("#contenedorOne");
+const boton = document.getElementById("agregar");
+
+
+console.log(nombre);
+
+
+boton.addEventListener("click", function(){
+    div1.append(nombre + " ");
+ 
+}) 
+
+/** 
+const divElement = document.createElement("div");
+divElement.textContent="hola";
+divElement.style.color="red";
+divElement.id="ElemId"
+
+
+
+const itemNombre = document.createElement(nombre);
+itemNombre.style.color="red";
+itemNombre.id="Juan"
+div1.appendChild(itemNombre);
+
+
+setTimeout(function(){
+   let elementDelete = div1.querySelector("itemNombre");
+   div1.removeChild(elementDelete)
+}, 3000);
+
+*/
