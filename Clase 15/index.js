a8399a1dbac96068bc4912d9e819aae9f0d98264
vero <veroo.corvalan@gmail.com>
2023-07-19 21:49:35 -0300

    Update index.js

--- Clase 15/index.js
+++ Clase 15/index.js
@@ -9,38 +9,8 @@ console.log(nombre);
 
 boton.addEventListener("click", function(){
     div1.append(nombre + " ");
-    Swal.fire({
-        position: 'top-center',
-        icon: 'success',
-        title: 'tuki',
-        background: '#fff5ee',
-        showConfirmButton: false,
-        timer: 1500
-      })
-    
-  
- 
+
+
 }) 
-
-/** 
-const divElement = document.createElement("div");
-divElement.textContent="hola";
-divElement.style.color="red";
-divElement.id="ElemId"
-
-
-
-const itemNombre = document.createElement(nombre);
-itemNombre.style.color="red";
-itemNombre.id="Juan"
-div1.appendChild(itemNombre);
-
-
-setTimeout(function(){
-   let elementDelete = div1.querySelector("itemNombre");
-   div1.removeChild(elementDelete)
-}, 3000);
-
-*/
 
 
