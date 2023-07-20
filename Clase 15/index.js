864059ef7c8d03fd4b940e09e32141f3eccf3b30
vero <veroo.corvalan@gmail.com>
2023-07-19 22:32:22 -0300

    v
    v

--- Clase 15/index.js
+++ Clase 15/index.js
@@ -1,16 +1,37 @@
+const inputItems = document.querySelector("#inputItems");
+const btnItems = document.getElementById("btnItems");
+const listItems= document.querySelector("#listItems");
 
-let nombre = document.getElementById("nombre").value;
-const div1 = document.querySelector("#contenedorOne");
-const boton = document.getElementById("agregar");
+inputItems.focus();
+
+btnItems.addEventListener("click",function(){
+ //   alert(inputItems.value);
+ const trElement = document.createElement("tr"); /** <tr></tr> */
+ const tdElementOne = document.createElement("td");/**<td></td> */
+ const tdElementTwo = document.createElement("td");/**<td></td> */
+ const tdButton = document.createElement("button");
+    tdButton.innerText="Eliminar"
+   // tdButton.className="btn btn-danger"
+    tdButton.classList.add("btn","btn-danger");
+    
+tdButton.addEventListener("click", function(){
+trElement.removeChild(tdElementTwo);
+trElement.removeChild(tdElementOne);
+
+})
+    
+ tdElementOne.textContent = inputItems.value;
+ tdElementTwo.appendChild(tdButton);
+ //tdElementTwo.innerHTML ="<button class='btn-dark btn'>Eliminar</button>"
+
+ trElement.appendChild(tdElementOne);/** <tr><td></td></tr> */
+ trElement.appendChild(tdElementTwo);/**<tr><td></td> <td></td></tr> */
+
+ listItems.prepend(trElement);
 
 
-console.log(nombre);
 
-
-boton.addEventListener("click", function(){
-    div1.append(nombre + " ");
-
-
-}) 
-
+ inputItems.focus();
+ inputItems.value="";
+})
 
